e26d53504fa3cef1f7a6f857995c3345c5ec0cab
Krystian Zmuda <krystian.zmuda@globallogic.com>
2021-10-03 20:50:18 +0200

    correct

--- src/main/java/masterclass/section7/Hamburger.java
+++ src/main/java/masterclass/section7/Hamburger.java
@@ -11,16 +11,43 @@ public class Hamburger {
         this.meat = meat;
         this.price = price;
         this.breadRollType = breadRollType;
+        System.out.println(this.name + " hamburger on a " + this.breadRollType + " roll with " + this.meat + ", price is " + this.price);
     }
-    String addition1Name;
-    double addition1Price;
-    String addition2Name;
-    double addition2Price;
-    String addition3Name;
-    double addition3Price;
-    String addition4Name;
-    double addition4Price;
+
+//    String addition2Name;
+//    double addition2Price;
+//    String addition3Name;
+//    double addition3Price;
+//    String addition4Name;
+//    double addition4Price;
 
 
-    public 
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        this.price += addition1Price;
+        System.out.println("Added " + addition1Name + " for an extra " + addition1Price);
+    }
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        this.price += addition2Price;
+        System.out.println("Added " + addition2Name + " for an extra " + addition2Price);
+    }
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        this.price += addition3Price;
+        System.out.println("Added " + addition3Name + " for an extra " + addition3Price);
+    }
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        this.price += addition4Price;
+        System.out.println("Added " + addition4Name + " for an extra " + addition4Price);
+    }
+
+    public double itemizeHamburger() {
+        return this.price;
+    }
+
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+    }
 }
